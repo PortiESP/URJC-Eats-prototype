041d9ecb2a32555a9b9b13977b50772a696f07c2
PRTI <portiesp@gmail.com>
2024-05-05 01:21:43 +0200

    fixed mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,26 @@ import logo1 from '@/assets/madrid_dev.svg'
 import { useState } from 'react'
 import requirements from '@/data/requirements'
 import Requirement from './components/Requirement'
+import { useEffect } from 'react'
 
 function App() {
 
-  const [scene, setScene] = useState('login')
+  const [scene, setScene] = useState('mainMenu')
+  
+  useEffect(() => {
+    const setVH = () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    setVH()
+
+    window.addEventListener('resize', setVH)
+
+    return () => {
+      window.removeEventListener('resize', setVH)
+    }
+  }, [])
 
   return (
     <div className="app-wrapper">
