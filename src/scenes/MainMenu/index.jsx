9479bd02fa97a40894bf9035b080ac52daf71eae
PRTI <portiesp@gmail.com>
2024-05-04 17:25:53 +0200

    MainScene

--- src/scenes/MainMenu/index.jsx
+++ src/scenes/MainMenu/index.jsx
@@ -1,17 +1,14 @@
+import scss from '@/styles/scenes_styles/main_menu.module.scss';
+
 export default function MainMenu(){
     return (
-        <div className="main-menu-wrapper">
-        <div className="main-menu-top-container">
-            <h1>Game Title</h1>
-        </div>
-        <div className="main-menu-mid-container">
-            <div className='main-menu-image-container'>
-            <img src="/assets/iphone_frame.png"/>
+        <div className={scss.wrapper}>
+            <div className="main-menu-top-container">
+                <h1>Game Title</h1>
             </div>
-        </div>
-        <div className="main-menu-bottom-container">
-            <button>Start Game</button>
-        </div>
+            <div className="main-menu-bottom-container">
+                <button>Start Game</button>
+            </div>
         </div>
     )
 }
