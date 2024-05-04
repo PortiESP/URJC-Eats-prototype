3555a0b9bdb5824dc2c75b58d42ef97ae9660111
PRTI <portiesp@gmail.com>
2024-05-05 00:34:55 +0200

    Example req

--- src/data/requirements.js
+++ src/data/requirements.js
@@ -5,7 +5,14 @@ const requirements = {
             title: "Autenticación de usuario",
             description: "El sistema debe permitir al usuario autenticarse"
         }
-    ]
+    ],
+    "mainMenu": [
+        {
+            code: "RF-02",
+            title: "Menú principal",
+            description: "El sistema debe mostrar un menú principal"
+        }
+    ],
 }
 
 export default requirements;
