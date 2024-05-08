372a8050789c8e60e090214d8d53789490fb125f
Porti <admin@0xPorti.com>
2024-05-08 16:40:34 +0200

    typos

--- src/data/requirements.js
+++ src/data/requirements.js
@@ -1,7 +1,7 @@
 const requirements = {
     "welcome": [
         {
-            title: "ℹ️ Esto es un prototipo, no una aplicación real",
+            title: "Esto es un prototipo, no una aplicación real",
             description: "Este prototipo ha sido creado con el fin de mostrar una posible aplicación de gestión de pedidos de comida para la URJC. No se trata de una aplicación real, por lo que la mayoría de las funcionalidades no están implementadas, y la información mostrada es ficticia. El objetivo de este prototipo es mostrar cómo podría ser la aplicación final, y recoger feedback del cliente y validar los requisitos para poder realizar los cambios necesarios antes de comenzar con el desarrollo de la aplicación real."
         }
     ],
